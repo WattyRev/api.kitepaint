908df376653fc45e1c8f0f922ac7791fdf633bde
Spencer Watson <wattyrev@gmail.com>
2018-09-09 19:59:42 -0700

    PHP cors

--- api/php/admin/header.php
+++ api/php/admin/header.php
@@ -9,6 +9,22 @@ require_once ("../functions.inc.php"); // include all the functions
 $seed = "0dAfghRqSTgx"; // the seed for the passwords
 $domain =  "kitepaint.com"; // the domain name without http://www.
 
+// Allow other domains access
+$allowedOrigins = array(
+  '(http(s)://)?(admin\.|admin\.beta\.)?kitepaint.com'
+);
+if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+  foreach ($allowedOrigins as $allowedOrigin) {
+    if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+      header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+      header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+      header('Access-Control-Max-Age: 1000');
+      header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+      break;
+    }
+  }
+}
+
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
