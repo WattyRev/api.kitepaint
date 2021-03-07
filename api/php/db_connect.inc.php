643684c8a80ab73cf158be47aa334fc275621dae
Spencer Watson <wattyrev@gmail.com>
2021-03-07 10:15:27 -0800

    try using global connection

--- api/php/db_connect.inc.php
+++ api/php/db_connect.inc.php
@@ -14,6 +14,6 @@ $isProduction = (strpos($_SERVER['HTTP_HOST'], 'beta') === false);
 define("DB", $isProduction ? "r3vfan_kite_paint" : "r3vfan_kite_paint_beta");
 
 ############## Make the mysql connection ###########
-$conn = mysqli_connect(HOST, DBUSER, PASS, DB) or  die('Could not connect to mySQL!<br />Please contact the site\'s administrator.');
+define("CONNECTION", mysqli_connect(HOST, DBUSER, PASS, DB) or  die('Could not connect to mySQL!<br />Please contact the site\'s administrator.'));
 
 ?>
--- api/php/user.functions.inc.php
+++ api/php/user.functions.inc.php
@@ -139,8 +139,8 @@ function user_exists($username) {
         return false;
     }
     var_dump($username);
-    var_dump($conn);
-    $escapedUsername = mysqli_real_escape_string($conn, $username);
+    var_dump(CONNECTION);
+    $escapedUsername = mysqli_real_escape_string(CONNECTION, $username);
     var_dump($escapedUsername);
 
     $query = sprintf("SELECT loginid FROM login WHERE username = '%s' LIMIT 1",
@@ -148,7 +148,7 @@ function user_exists($username) {
 
     var_dump($query);
 
-    $result = mysqli_query($conn, $query);
+    $result = mysqli_query(CONNECTION, $query);
 
     var_dump($result);
 
