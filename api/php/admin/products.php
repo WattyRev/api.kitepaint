7fe33a90022d164301137d006231968c0314ecfc
Spencer Watson <wattyrev@gmail.com>
2021-11-19 13:15:32 -0800

    fix typo

--- api/php/admin/products.php
+++ api/php/admin/products.php
@@ -172,7 +172,7 @@ function updateProduct($postData) {
     			$response->message = 'Unable to update variation ' . $variationId;
     		}
         } else {
-            $resoonse->message = 'Creating a new variation';
+            $response->message = 'Creating a new variation';
             // Create new variation
             $sql = sprintf("INSERT INTO variations (name,svg,productId,sortIndex) value ('%s','%s','%s','%s')", mysqli_real_escape_string($conn, $variation->name), mysqli_real_escape_string($conn, $variation->svg), mysqli_real_escape_string($conn, $id), mysqli_real_escape_string($conn, $index));
         }
