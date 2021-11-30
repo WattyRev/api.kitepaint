d18ec75efc71c473460e903e85ac2df8d44c654d
Spencer Watson <wattyrev@gmail.com>
2021-11-30 13:10:26 -0800

    Remove unused billing fields from admin (#21)

--- api/php/admin/manufacturers.php
+++ api/php/admin/manufacturers.php
@@ -16,14 +16,7 @@ if ($_GET){
 		$manufacturer->contact_name = mysqli_result($result,$i,"contact_name");
 		$manufacturer->contact_phone = mysqli_result($result,$i,"contact_phone");
 		$manufacturer->contact_email = mysqli_result($result,$i,"contact_email");
-		$manufacturer->billing_name = mysqli_result($result,$i,"billing_name");
-		$manufacturer->billing_phone = mysqli_result($result,$i,"billing_phone");
 		$manufacturer->billing_email = mysqli_result($result,$i,"billing_email");
-		$manufacturer->billing_address = mysqli_result($result,$i,"billing_address");
-		$manufacturer->billing_city = mysqli_result($result,$i,"billing_city");
-		$manufacturer->billing_state = mysqli_result($result,$i,"billing_state");
-		$manufacturer->billing_postal = mysqli_result($result,$i,"billing_postal");
-		$manufacturer->billing_country = mysqli_result($result,$i,"billing_country");
 		$manufacturer->invoice_amount = mysqli_result($result,$i,"invoice_amount");
 		$manufacturer->last_paid = date("m/d/Y", strtotime(mysqli_result($result,$i,"last_paid")));
 		$manufacturer->products = mysqli_result($result,$i,"products");
@@ -80,14 +73,7 @@ if ($_GET){
 		$contact_name = $_POST['contact_name'];
 		$contact_phone = $_POST['contact_phone'];
 		$contact_email = $_POST['contact_email'];
-		$billing_name = $_POST['billing_name'];
-		$billing_phone = $_POST['billing_phone'];
 		$billing_email = $_POST['billing_email'];
-		$billing_address = $_POST['billing_address'];
-		$billing_city = $_POST['billing_city'];
-		$billing_state = $_POST['billing_state'];
-		$billing_postal = $_POST['billing_postal'];
-		$billing_country = $_POST['billing_country'];
 		$invoice_amount = $_POST['invoice_amount'];
 		$logo = $_POST['logo'];
 		$website = $_POST['website'];
@@ -99,19 +85,12 @@ if ($_GET){
 			echo json_encode($response);
 			return;
 		}
-		$sql = sprintf("insert into manufacturers (activated,created,name,contact_name,contact_phone,contact_email,billing_name,billing_phone,billing_email,billing_address,billing_city,billing_state,billing_postal,billing_country,invoice_amount,last_paid,logo,website) value (1, now(), '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', now(), '%s', '%s')",
+		$sql = sprintf("insert into manufacturers (activated,created,name,contact_name,contact_phone,contact_email,billing_email,invoice_amount,last_paid,logo,website) value (1, now(), '%s', '%s', '%s', '%s', '%s', '%s', now(), '%s', '%s')",
 		mysqli_real_escape_string($conn, $name),
 		mysqli_real_escape_string($conn, $contact_name),
 		mysqli_real_escape_string($conn, $contact_phone),
 		mysqli_real_escape_string($conn, $contact_email),
-		mysqli_real_escape_string($conn, $billing_name),
-		mysqli_real_escape_string($conn, $billing_phone),
 		mysqli_real_escape_string($conn, $billing_email),
-		mysqli_real_escape_string($conn, $billing_address),
-		mysqli_real_escape_string($conn, $billing_city),
-		mysqli_real_escape_string($conn, $billing_state),
-		mysqli_real_escape_string($conn, $billing_postal),
-		mysqli_real_escape_string($conn, $billing_country),
 		mysqli_real_escape_string($conn, $invoice_amount),
 		mysqli_real_escape_string($conn, $logo),
 		mysqli_real_escape_string($conn, $website));
@@ -135,14 +114,7 @@ if ($_GET){
 		'contact_name' => $_POST['contact_name'],
 		'contact_phone' => $_POST['contact_phone'],
 		'contact_email' => $_POST['contact_email'],
-		'billing_name' => $_POST['billing_name'],
-		'billing_phone' => $_POST['billing_phone'],
 		'billing_email' => $_POST['billing_email'],
-		'billing_address' => $_POST['billing_address'],
-		'billing_city' => $_POST['billing_city'],
-		'billing_state' => $_POST['billing_state'],
-		'billing_postal' => $_POST['billing_postal'],
-		'billing_country' => $_POST['billing_country'],
 		'invoice_amount' => $_POST['invoice_amount'],
 		'logo' => $_POST['logo'],
 		'website' => $_POST['website'],
